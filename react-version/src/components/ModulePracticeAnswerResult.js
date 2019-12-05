0a7de090bfb6793131078c2e589de410fc63f5a8
enfrte <enfrte@gmail.com>
2019-12-05 14:06:43 +0200

    Added styles to answer results and refactored that part of the code

--- react-version/src/components/ModulePracticeAnswerResult.js
+++ react-version/src/components/ModulePracticeAnswerResult.js
@@ -10,6 +10,8 @@ const ModulePracticeAnswerResult = ( {questionNumber, answer, attempt} ) => {
   const { selectedPracticeEnd } = useContext(PracticeContext);
 
   const [checkAnswer, setCheckAnswer] = useState(false);
+  const [resultMessage, setResultMessage] = useState('');
+  const [resultAnswer, setResultAnswer] = useState('');
 
   // create the user selected module for practice
   useEffect(() => {
@@ -21,13 +23,6 @@ const ModulePracticeAnswerResult = ( {questionNumber, answer, attempt} ) => {
     e.preventDefault();
     if (checkAnswer === false) {
       setCheckAnswer(true);
-      if (attempt === answer) {
-        console.log('Correct');
-        // set styles
-      }
-      else {
-        // set incorrect styles
-      }
       return; 
     }
     if (selectedPracticeEnd === true) {
@@ -41,21 +36,56 @@ const ModulePracticeAnswerResult = ( {questionNumber, answer, attempt} ) => {
     setQuestionIndex(questionNumber + 1);
   }
   
-  return (
-    <div className="module-practice-answer-result">
-      
-      {/* TO DO: 
-        set a conditional diabled flag in the button markup based on the absence of answer data in the answer area */}
+  useEffect(() => {
+    // when check answer button has been pressed, its state changes to false until the continue button is pressed
+    if (checkAnswer === true) {
+      // display the result of the answer
+      if (answer === attempt) {
+        setResultMessage('Correct');
+        setResultAnswer('');
+      } 
+      else {
+        setResultMessage('Incorrect');
+        setResultAnswer(answer);
+      }
+      return;
+    }
+  }, [checkAnswer, answer, attempt]);
 
-      <button 
-        className={ checkAnswer === false ? 'answer-button answer-button-default' : 'answer-button answer-button-continue' } 
-        onClick={progress} disabled={attempt < 1 ? 'disabled' : ''}>
-          { checkAnswer === false ? 'Check' : 'Continue' }
-      </button>
-      
-      <ModuleFinished />
-    </div>
-  );
+  if ( checkAnswer === false ) {
+    return (
+      <div className="module-practice-answer-check-area">
+        <button 
+          style={attempt.length < 1 ? {} : {color: 'white', background: '#4CAF50'}} 
+          className={ checkAnswer === false ? 'answer-button answer-button-default' : 'answer-button answer-button-continue' } 
+          onClick={progress} disabled={attempt.length < 1 ? 'disabled' : ''}>Check Answer</button>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="module-practice-answer-result-area">
+        <div style={ answer === attempt ? { backgroundColor: 'rgb(99, 217, 104)', color: 'rgb(48, 113, 51)' } : { backgroundColor: 'rgb(255, 160, 176)', color: 'rgb(196, 0, 33)' } } className="module-practice-answer-result">
+
+          <button 
+            style={ answer === attempt ? { backgroundColor: 'rgb(48, 113, 51)', color: 'white' } : { backgroundColor: 'rgb(196, 0, 33)', color: 'white' } } 
+            className="answer-button"
+            onClick={ progress }>Continue
+          </button>
+
+          <div className="result-info-container">
+            <div className="result-info">
+              <h4 style={ answer === attempt ? { color: 'rgb(48, 113, 51)' } : { color: 'rgb(196, 0, 33)' } }>{ resultMessage }</h4>
+              <p>{ answer === attempt ? '' : <strong>Answer: </strong> } { resultAnswer }</p>
+            </div>
+          </div>
+
+        </div>
+        <ModuleFinished />
+      </div>
+    );
+  }
+
 }
 
 export default ModulePracticeAnswerResult;
