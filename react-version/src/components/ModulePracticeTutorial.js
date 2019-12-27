4f0dd9a6f1ba9129f88b0f80551300ef48ddefa0
enfrte <enfrte@gmail.com>
2019-12-28 01:05:45 +0200

    Added ReactHtmlParser module to safely render html coming from json

--- react-version/src/components/ModulePracticeTutorial.js
+++ react-version/src/components/ModulePracticeTutorial.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import { PracticeContext } from '../contexts/PracticeContext';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 // Displays the question to the user
 const ModulePracticeTutorial = ( { module } ) => {
@@ -10,8 +11,8 @@ const ModulePracticeTutorial = ( { module } ) => {
       <button className="answer-button tutorial-button" onClick={ () => setShowPracticeTutorial(!showPracticeTutorial) }>Tutorial</button>
       <div className={ showPracticeTutorial ? 'module-practice-tutorial-screen' : 'hidden' }>
         <div className="module-practice-tutorial">
-          <h3>{module.title}</h3>
-          <p>{module.tutorial}</p>
+          <h2>{module.title}</h2>
+          <div>{ReactHtmlParser(module.tutorial)}</div>
           <button className="answer-button answer-button-continue" onClick={ () => setShowPracticeTutorial(!showPracticeTutorial) }>CLOSE TUTORIAL</button>
         </div>
       </div>
