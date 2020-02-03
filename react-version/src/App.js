eba5b39c7f6786bd8cc8c00ccdeb80cf30e700cf
enfrte <enfrte@gmail.com>
2020-02-03 11:50:02 +0200

    Removed old routes from App.js

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './App.css';
 import PracticeContextProvider from './contexts/PracticeContext';
-import ModulePractice from './components/ModulePractice';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Menu from './pages/Menu';
-import Practice from './pages/Practice';
 
 function App() {
   return (
-    <Router>
-      <div className="app">      
-        <PracticeContextProvider>
-          <Menu />
-        </PracticeContextProvider>
-      </div>
-    </Router>
+    <div className="app">      
+      <PracticeContextProvider>
+        <Menu />
+      </PracticeContextProvider>
+    </div>
   );
 }
 
